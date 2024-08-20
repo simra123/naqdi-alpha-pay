7a0373dba56af4bf587481ab622f23169d90716b
shahzaib-khowaja <shahzaib.kkhowaja@gmail.com>
2024-08-20 20:34:06 +0500

    production pipline

---
+++ scripts_code_deploy/npm_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/app/alpha-portal-client-admin && sudo chown ubuntu:ubuntu -R *
+cd /home/ubuntu/app/alpha-portal-client-admin && sudo chown ubuntu:ubuntu -R .*
---
+++ scripts_code_deploy/pm2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/ubuntu/app/alpha-portal-client-admin && pm2 start
