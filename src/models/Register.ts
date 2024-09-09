097620aefc1c045a9936b79037bb7ea84a5e92cf
Ahmed <aw708596@gmail.com>
2024-09-09 21:28:18 +0500

    update password max length

--- src/models/Register.ts
+++ src/models/Register.ts
@@ -25,7 +25,7 @@ const _register = {
     .required("password is required")
     .min(8, "Password must be at least 8 characters")
     .matches(
-      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,24}$/,
       "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
     ),
 
