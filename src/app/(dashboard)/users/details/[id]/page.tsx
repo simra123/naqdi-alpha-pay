e63a560177fc1f57da64314e1c5961bb315f00bd
Ahmed <aw708596@gmail.com>
2024-10-02 23:19:30 +0500

    remove send code button

--- src/app/(dashboard)/users/details/[id]/page.tsx
+++ src/app/(dashboard)/users/details/[id]/page.tsx
@@ -44,102 +44,85 @@ const TransactionDetails = ({ params }) => {
   }, []);
 
   return (
-    <DashboardPageWrapper>
-      <div className="data-grid-container">
-        <div className=" flex items-center justify-between">
-          <h2 className="text-xl font-semibold">User Details</h2>
+    <div className="rounded-medium flex flex-col  bg-white p-6">
+      <h3 className="text-h3.5 font-semibold text-blackGrey-100 ">
+        User Details
+      </h3>
+      <ErrorApiText error={isUserDetailsError} />
+      <LoadingApi loading={isUserDetailsLoading}>
+        <div className="detailspage mt-6">
+          <div className="flex flex-col gap-4">
+            <DetailsWrapper title={"Date Created"}>
+              <TransparentInput
+                value={moment(userDetails?.user?.created_at).format(
+                  "DD-MM-YYYY"
+                )}
+              />
+            </DetailsWrapper>
+            <DetailsWrapper title={"ID"}>
+              <TransparentInput value={userDetails?.id} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"First Name"}>
+              <TransparentInput value={userDetails?.user?.first_name} />
+            </DetailsWrapper>
+            {userDetails?.user?.legal_name && (
+              <DetailsWrapper title={"Legal Name"}>
+                <TransparentInput value={userDetails?.user?.legal_name} />
+              </DetailsWrapper>
+            )}
+            <DetailsWrapper title={"Last Name"}>
+              <TransparentInput value={userDetails?.user?.last_name} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"Email"}>
+              <TransparentInput value={userDetails?.user?.email} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"Username"}>
+              <TransparentInput value={userDetails?.user?.username} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"User Type"}>
+              <TransparentInput value={userDetails?.user?.user_type} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"Status"}>
+              <TransparentInput
+                value={userDetails?.user?.verified ? "Verified" : "UnVerified"}
+              />
+            </DetailsWrapper>
+
+            <DetailsWrapper title={"Address"}>
+              <TransparentInput value={userDetails?.address_line_1} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"State"}>
+              <TransparentInput value={capitalize(userDetails?.state)} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"City"}>
+              <TransparentInput value={userDetails?.city} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"Country"}>
+              <TransparentInput value={userDetails?.country} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"Phone"}>
+              <TransparentInput value={userDetails?.phone_number} />
+            </DetailsWrapper>
+            <DetailsWrapper title={"Postal Code"}>
+              <TransparentInput value={userDetails?.postal_code} />
+            </DetailsWrapper>
+          </div>
         </div>
-        <ErrorApiText error={isUserDetailsError} />
-        <LoadingApi loading={isUserDetailsLoading}>
-          <div className="detailspage mt-6">
-            <div className="flex flex-col gap-4">
-              <DetailsWrapper title={"Date Created"}>
-                <TransparentInput
-                  value={moment(userDetails?.user?.created_at).format(
-                    "DD-MM-YYYY"
-                  )}
-                />
-              </DetailsWrapper>
-              <DetailsWrapper title={"ID"}>
-                <TransparentInput value={userDetails?.id} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"First Name"}>
-                <TransparentInput value={userDetails?.user?.first_name} />
-              </DetailsWrapper>
-              {userDetails?.user?.legal_name && (
-                <DetailsWrapper title={"Legal Name"}>
-                  <TransparentInput value={userDetails?.user?.legal_name} />
-                </DetailsWrapper>
-              )}
-              <DetailsWrapper title={"Last Name"}>
-                <TransparentInput value={userDetails?.user?.last_name} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"Email"}>
-                <TransparentInput value={userDetails?.user?.email} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"Username"}>
-                <TransparentInput value={userDetails?.user?.username} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"User Type"}>
-                <TransparentInput value={userDetails?.user?.user_type} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"Status"}>
-                <TransparentInput
-                  value={
-                    userDetails?.user?.verified ? "Verified" : "UnVerified"
-                  }
-                />
-              </DetailsWrapper>
-
-              <DetailsWrapper title={"Address"}>
-                <TransparentInput value={userDetails?.address_line_1} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"State"}>
-                <TransparentInput value={capitalize(userDetails?.state)} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"City"}>
-                <TransparentInput value={userDetails?.city} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"Country"}>
-                <TransparentInput value={userDetails?.country} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"Phone"}>
-                <TransparentInput value={userDetails?.phone_number} />
-              </DetailsWrapper>
-              <DetailsWrapper title={"Postal Code"}>
-                <TransparentInput value={userDetails?.postal_code} />
-              </DetailsWrapper>
-            </div>
+        <div className="data-grid-container mt-1">
+          <div className="tableheader  border border-b-0 py-6 px-3 flex items-center justify-between">
+            <h2 className="text-xl font-semibold">Wallets</h2>
           </div>
-          <div className="data-grid-container mt-1">
-            <div className="tableheader  border border-b-0 py-6 px-3 flex items-center justify-between">
-              <h2 className="text-xl font-semibold">Wallets</h2>
-            </div>
 
-            <DataGrid
-              rows={wallets}
-              columns={wallet_table_columns}
-              className="font-semibold primary-color border-t-0"
-              hideFooter
-              autoHeight
-            />
-          </div>
-          <div className="data-grid-container mt-1">
-            <div className="tableheader  border border-b-0 py-6 px-3 flex items-center justify-between">
-              <h2 className="text-xl font-semibold">Transactions</h2>
-            </div>
-
-            <DataGrid
-              rows={transactions}
-              columns={transactionsList_table_columns}
-              className="font-semibold primary-color border-t-0"
-              hideFooter
-              autoHeight
-            />
-          </div>
-        </LoadingApi>
-      </div>
-    </DashboardPageWrapper>
+          <DataGrid
+            rows={wallets}
+            columns={wallet_table_columns}
+            className="font-semibold primary-color border-t-0"
+            hideFooter
+            autoHeight
+          />
+        </div>
+      </LoadingApi>
+    </div>
   );
 };
 
