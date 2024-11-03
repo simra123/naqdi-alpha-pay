982c743b7826ef7b63ec0798a6bb7cffffc91bfd
Ahmed <aw708596@gmail.com>
2024-11-03 21:47:47 +0500

    fixed and removed network field in withdrawal

--- src/models/withdrawal.ts
+++ src/models/withdrawal.ts
@@ -4,7 +4,6 @@ import * as Yup from 'yup';
 export const getWithdrawalSchema = (network: boolean, maxSourceAmount: number) => {
     return Yup.object().shape({
         blockchain: Yup.string().required("Blockchain is required"),
-        network: network ? Yup.string().required() : Yup.string().notRequired(),
         token: Yup.string().required("Token is required").length(6, "Token must be exactly 6 characters long"),
         amount: Yup.number().transform((value, originalValue) => (originalValue === '' ? undefined : value)).min(0.0001).max(maxSourceAmount, `Source amount must be equal or less than ${maxSourceAmount}`).required("Source amount is required"),
         recipient_address : Yup.string().required("Wallet address is required"),
