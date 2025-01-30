c9a6944229a18e97b4c472fc0a91d26c7643df6b
Ahmed <aw708596@gmail.com>
2025-01-30 15:09:14 +0500

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  images: {
+    domains: ["alpha-pay-assets-staging.s3.ap-southeast-1.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
