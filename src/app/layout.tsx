3af4f7dd9f73b422c0b2543cf1ab3a39d9f36195
Ahmed <muhammad.ahmed@maleyat.com>
2025-03-15 17:44:46 +0500

    added favs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,38 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo-small.png"/>
+        <title>Alphaspay - Crypto Gateway</title>
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
       </head>
       <body className={`${lato.variable} ${nunitoSans.variable}`}>
         <Provider store={store}>
