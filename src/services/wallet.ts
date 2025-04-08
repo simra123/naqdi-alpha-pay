840ffc3b3c6d0445648e44d2137acf2ceb09eb59
Ahmed <muhammad.ahmed@maleyat.com>
2025-04-08 13:04:05 +0500

    adding merchant details

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -13,6 +13,10 @@ export const getAllWalletBalancesApi = () => {
   return () => api.get(`wallet/balance`);
 };
 
+export const getAllAdminWalletBalancesApi = () => {
+  return () => api.get(`admin/wallet/balance`);
+};
+
 export const getAllWalletsListByAdminApi = () => {
   return () => api.get(`wallet/wallet-list`);
 };
