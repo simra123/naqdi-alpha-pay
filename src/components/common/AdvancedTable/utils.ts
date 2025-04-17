ab13eeadcd38fa6bd1d43ddf5454572f32a9601e
Ahmed <muhammad.ahmed@maleyat.com>
2025-04-17 17:15:20 +0500

    fixed table and path

--- src/components/common/AdvancedTable/utils.ts
+++ src/components/common/AdvancedTable/utils.ts
@@ -149,22 +149,48 @@ export const formatListViewsFromGroups = (data, views) => {
   ); // Flatten meta arrays from all groups
 };
 
+// export const updateMetaItemInGroup = (
+//   groups: any[],
+//   metaNames: string[],
+//   newProperties: {
+//     operator: any;
+//     values: any[];
+//     listColumnMeta: { name: string };
+//     isSelected?: boolean;
+//   }
+// ) => {
+//   return groups.map((group) => {
+//     return {
+//       ...group,
+//       meta: group.meta.map((metaItem) => {
+//         if (metaNames.includes(metaItem.name)) {
+//           return { ...metaItem, ...newProperties };
+//         }
+//         return metaItem;
+//       }),
+//     };
+//   });
+// };
+
 export const updateMetaItemInGroup = (
   groups: any[],
-  metaNames: string[],
-  newProperties: {
-    operator: any;
-    values: any[];
-    listColumnMeta: { name: string };
-    isSelected?: boolean;
-  }
+  updates: Record<
+    string,
+    {
+      operator: any;
+      values: any[];
+      listColumnMeta: { name: string };
+      isSelected?: boolean;
+    }
+  >
 ) => {
   return groups.map((group) => {
     return {
       ...group,
       meta: group.meta.map((metaItem) => {
-        if (metaNames.includes(metaItem.name)) {
-          return { ...metaItem, ...newProperties };
+        const update = updates[metaItem?.name];
+        if (update) {
+          return { ...metaItem, ...update };
         }
         return metaItem;
       }),
@@ -247,7 +273,6 @@ export const getPaginationPages = (
 
   const showLeftEllipsis = leftSiblingIndex > 2;
   const showRightEllipsis = rightSiblingIndex < totalPages - 1;
-
 
   if (showLeftEllipsis) {
     pages.push("..."); // Left ellipsis
